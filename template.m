bf9816ced2a1a4526f8d4386929881077ec1bcf7
doneamo <jennifertan2009@gmail.com>
2020-10-25 19:02:06 -0700

    Create template.m

---
+++ template.m
@@ -0,0 +1,20 @@
+% File name: template.m
+% Author: 
+% Date: 
+
+% Description: 
+
+
+function [] = template()
+
+    
+    
+end
+
+%removes all new line characters and replaces them with a space
+function [output] = methodName (input)
+
+    output = input;
+
+end
+
