0c481343a127c7ad5192074c2508edc1f3bfb2f1
Antremblay <73318863+Antremblay@users.noreply.github.com>
2020-10-31 13:16:39 -0400

    Update WingLoading.m

--- WingLoading.m
+++ WingLoading.m
@@ -1,20 +1,24 @@
 
-% Plot lift distribution for Q4.1
  m = 591;                %Nombre de point de donné
  Cr=17.15;               %Corde à l'emplenture [ft]
  Ct=10.3;                %Corde au bout d'aile [ft]
  nUltPos=3.25*1.5;           %Facteur de chargement limite positif
  b=118;                  %Envergure [ft]
- W=121368;               %Poids [lbf]
- B=2*W*nUltPos/b/(Cr+Ct);
- pente=(Ct-Cr)*2/b;
+ W=121904;               %Poids [lbf]
+ Wwing=7776/2;              %Poids de une aile [lbf]  
+ Wfuel=27162.5;           %Poids du gaz pour une aile [lbf]
+ Dfuselage=15.08;        %Diamètre du fuselage
+ WeightMotor=971;         %Poids d'un moteur [lbf]
+ RadiusMotor=8.75;        %Diamètre des hélices
+ yMoteur1=1*RadiusMotor+Dfuselage/2;
+ yMoteur2=3*RadiusMotor+Dfuselage/2;
  
  %Matrice de l'envergure
  y = zeros(1,m);
  for i = 1:1:m
      y(1,i) = -b/2/(m-1) + b/2/(m-1)*i;
  end
- disp(y)
+
 
   %Generate Elliptical Distribution
  Ellipse = zeros(1,m);
@@ -24,8 +28,10 @@
  
  %Generate Trapezoidal Distribution
  Trap = zeros(1,m);
+ B=2*W*nUltPos/b/(Cr+Ct);
+ pente=(Ct-Cr)*2/b;
  for d = 1:1:m
-     Trap(1,d) = B*(pente*y(1,d) + Cr);%
+     Trap(1,d) = B*(pente*y(1,d) + Cr);
  end
  
  % Generate lift vector with Shrenks approximation
@@ -33,7 +39,7 @@
  for j = 1:1:m
      L(1,j) = (Ellipse(1,j)+Trap(1,j))/2;
  end
- disp(L)
+
  
  %  %Close off the shrenk distribution
  close = zeros(1,2);
@@ -58,9 +64,6 @@
 
 
 %%%%%%%% Substracting the sutructure weight and the fuel weight%%%%%
-Wwing=7747    %Poids des ailes [lbf]    les deux ailes
-
-B1=-2*nUltPos*Wwing/(Cr+Ct)/b
 
  %Poids linéaire de la structure
  PoidsL = zeros(1,m);
@@ -68,16 +71,41 @@ B1=-2*nUltPos*Wwing/(Cr+Ct)/b
      PoidsL(1,g) = -2*nUltPos*Wwing/b/(Ct+Cr)*(2*(Ct-Cr)/b*y(1,g) + Cr);
  end
  
- %Poids linéaire de l'essence
+  %Poids linéaire de l'essence
+  FuelW = zeros(1,m);
+  for h = 1:1:m
+      if (y(1,h)>8.5) && (y(1,h)<52)      %La valeur minimale est supposé 1ft après le diamètre 
+                                          %et la valeur maximale 6ft avant le bout d'aile
+      FuelW(1,h) = -2*nUltPos*Wfuel/b/(Ct+Cr)*(2*(Ct-Cr)/b*y(1,h) + Cr);
+      else
+      FuelW(1,h)=0;
+      end
+  end
+ 
+%Substract the linear weight of the structure and the fuel from the
+%shrenk's lift aproximation
+
+ TrueLinearShearForce = zeros(1,m);
+ for o = 1:1:m
+     TrueLinearShearForce(1,o) = L(1,o)+PoidsL(1,o)+FuelW(1,o);
+ end
+ 
+ 
+ 
  
  
  
  %%%%%%%%%%%%%%%%% Plotting section %%%%%%%%%%%%%%%%
+ figure(1)
   plot(y,L,'k')
   hold on
   plot(close,L_close,'k')
   hold on
   plot(y,PoidsL)
+  hold on
+  plot(y,FuelW)
+  hold on
+  plot(y, TrueLinearShearForce)
   hold on
 %  plot(y,Ellipse,'r')
 %  hold on
