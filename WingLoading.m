7b3e8228f4f490c5cf8ed43fc3d2000743e875f0
Antremblay <73318863+Antremblay@users.noreply.github.com>
2020-11-01 15:20:51 -0500

    Update WingLoading.m

--- WingLoading.m
+++ WingLoading.m
@@ -7,8 +7,11 @@
  W=121904;               %Poids [lbf]
  Wwing=7776/2;              %Poids de une aile [lbf]  
  Wfuel=27162.5;           %Poids du gaz pour une aile [lbf]
- Dfuselage=15.08;        %Diamètre du fuselage
- WeightMotor=971;         %Poids d'un moteur [lbf]
+ Dfuselage=15.08;           %Diamètre du fuselage
+ WeightEngine=7768;         %Poids d'un moteur [lbf]
+ Wpropeller=15560    
+ Wpropulsion=3351
+ WeightPropSyst=(WeightEngine+Wpropeller+Wpropulsion)/4            %Only 1 unit
  RadiusMotor=8.75;        %Diamètre des hélices
  yMotor1=1*RadiusMotor+Dfuselage/2;
  yMotor2=3*RadiusMotor+Dfuselage/2;
@@ -95,9 +98,9 @@
  for p = m-1:-1:1
      ShearForce(1,p) = TrueLinearShearForce(1,p)*b/2/(m-1)+ShearForce(1,p+1);
      if round(yMotor1,1)==y(1,p)
-         ShearForce(1,p)=ShearForce(1,p)-2*WeightMotor*nUltPos;
+         ShearForce(1,p)=ShearForce(1,p)-2*WeightPropSyst*nUltPos;
      elseif round(yMotor2,1)==y(1,p)
-         ShearForce(1,p)=ShearForce(1,p)-2*WeightMotor*nUltPos;
+         ShearForce(1,p)=ShearForce(1,p)-2*WeightPropSyst*nUltPos;
      end
  end
  
@@ -145,7 +148,3 @@ Moment = zeros(1,m);
    grid minor
  xlabel('Wing Span ,y [ft]')
  ylabel('Moment, [lb_f] ft')
-
-testeste
-
-testestestest
