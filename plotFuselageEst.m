4b1b137970c1b413bbd5e20cae4d0e65937f6dba
doneamo <jennifertan2009@gmail.com>
2021-01-24 19:09:33 -0800

    Update plotFuselageEst.m

--- plotFuselageEst.m
+++ plotFuselageEst.m
@@ -1,19 +1,28 @@
 %calls fuselage boom estimate function and plots it
 
-My = 4e6;
-r = 7.5;
-Fty = 50;
-testCases = 20;
+%varaibles
+My = 4e6; %lbf ft
+r = 7.5; %ft
+Fty = 50; %ksi
+testCases = 10;
+density = 0.1; %lb/in^3, density of material
+
 numBooms = zeros(1,testCases);
 areas = zeros(1,testCases);
+weight = zeros(1,testCases);
 
+%calc area of booms req
 for i = 1:1:testCases
     numBooms(i) = i*4;
 end
 
 for i = 1:1:testCases
-    areas(i) = fuselageBoomEst(My,r,Fty,numBooms(i));
+    areas(i) = fuselageBoomEst(My,r,Fty,numBooms(i)); %in^2
+    
+    %calc total weight req
+    weight(i) = density*areas(i); %lb/in
 end
+
 
 %graph
 figure(1)
@@ -23,6 +32,11 @@ grid on
 xlabel('Number of Booms')
 ylabel('Area of Booms, [in^2]')
     
-
+figure(2)
+plot(numBooms, weight, 'k');
+hold on
+grid on
+xlabel('Number of Booms')
+ylabel('Weight Per Boom, [lb_f/in]')
 
 
