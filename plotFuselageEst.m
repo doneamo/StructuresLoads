0d004a9b160c00afda7978094da7aa0ca89de674
doneamo <jennifertan2009@gmail.com>
2021-02-08 21:26:52 -0800

    Update plotFuselageEst.m

--- plotFuselageEst.m
+++ plotFuselageEst.m
@@ -1,10 +1,11 @@
 %calls fuselage boom estimate function and plots it
 
 %varaibles
-My = 2290000; %lbf ft
+My = 2290000; %lbf ft ultimate load case
+%My = 1687830; %lbf ft limit load case
 r = 15.833/2; %ft,avg of inner and outer
 Ft = 62; %ksi
-testCases = 10;
+testCases = 80;
 density = 0.097; %lb/in^3, density of material
 
 numBooms = zeros(1,testCases);
@@ -26,7 +27,6 @@ end
 %graph
 figure(1)
 plot(numBooms, areas, 'k')
-hold on
 grid on
 xlabel('Nombre de Booms') %number of booms
 ylabel('Surface des Booms, [in^2]')%area of booms
@@ -35,7 +35,6 @@ ylabel('Surface des Booms, [in^2]')%area of booms
     
 figure(2)
 plot(numBooms, weight, 'k');
-hold on
 grid on
 xlabel('Number of Booms')
 ylabel('Weight Per Boom, [lb_f/in]')
@@ -43,19 +42,22 @@ ylabel('Weight Per Boom, [lb_f/in]')
 %=========================================================================
 
 %calc thickness for selected num of booms
-booms = 8;
+booms = 80;
+r_in = r *12;
 [area,z,theta] = fuselageBoomEst (My, r, Ft, booms);
 areaArrayPos = booms/4;
 A_max = areas(areaArrayPos); %boom area with 8 booms -> depends on booms
 A_min = areas(areaArrayPos+1); %boom area with 12 booms
-[thickness, areaSt] = skinThicknessEst(z, theta, r, area, A_max, A_min);
+[thickness, areaSt] = skinThicknessEst(z, theta, r_in, area, A_max, A_min);
 
 %plot thickness cruve
 figure(3)
 plot(thickness,areaSt, 'k-');
-hold on
 grid on
-xlabel('Épaisseur du Revêtement, [in]') %skin thickness
-ylabel('Surface des Raidisseurs , [in^2]') %area of stiffeners
+% xlabel('Épaisseur du Revêtement, [in]') %skin thickness
+% ylabel('Surface des Raidisseurs , [in^2]') %area of stiffeners
+xlabel('Skin Thickness, [in]') %skin thickness
+ylabel('Area of Stiffeners , [in^2]') %area of stiffeners
+
 
 
