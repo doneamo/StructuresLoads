57d4aa39cab0fd94f021d20c7f6334e56c137427
doneamo <jennifertan2009@gmail.com>
2021-02-09 20:59:58 -0800

    Update plotFuselageEst.m

--- plotFuselageEst.m
+++ plotFuselageEst.m
@@ -1,8 +1,8 @@
 %calls fuselage boom estimate function and plots it
 
 %varaibles
-My = 2290000; %lbf ft ultimate load case
-%My = 1687830; %lbf ft limit load case
+%My = 2290000; %lbf ft ultimate load case
+My = 1709000; %lbf ft limit load case
 r = 15.833/2; %ft,avg of inner and outer
 Ft = 62; %ksi
 testCases = 80;
@@ -42,7 +42,7 @@ ylabel('Weight Per Boom, [lb_f/in]')
 %=========================================================================
 
 %calc thickness for selected num of booms
-booms = 80;
+booms = 48;
 r_in = r *12;
 [area,z,theta] = fuselageBoomEst (My, r, Ft, booms);
 areaArrayPos = booms/4;
