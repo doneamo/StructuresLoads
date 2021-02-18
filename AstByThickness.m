ff045c16c3126b4091805bdda8f58bf6bc39416e
doneamo <jennifertan2009@gmail.com>
2021-02-17 17:17:07 -0800

    Create AstByThickness.m

---
+++ AstByThickness.m
@@ -0,0 +1,10 @@
+% calculate the area of the stringer based on the skin thickness required
+% z in inches
+% t in inches
+% b in inches
+% Aeq in in^2
+
+function [Ast] = AstByThickness(t,b,z,Aeq)
+    Ast = Aeq - ((t*b)/6)*(4 + z(3)/z(2) + z(1)/z(2));
+end
+
