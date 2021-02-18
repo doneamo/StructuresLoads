ee432c49f8c0e25b651e223547a4e00d6b9ac652
doneamo <jennifertan2009@gmail.com>
2021-02-17 22:26:35 -0800

    Update skinThicknessEst.m

--- skinThicknessEst.m
+++ skinThicknessEst.m
@@ -6,7 +6,6 @@
 
 function [t,A_st,b] = skinThicknessEst(z, theta, r, A_eq, A_max, A_min)
     b = r * deg2rad(theta); %arc length between booms
-    r
     b
     A_count = A_min;
     numElements = 0;
