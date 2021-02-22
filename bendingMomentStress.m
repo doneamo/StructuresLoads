de8ee3d4e7715977ca0170cf282ab825ee006e99
doneamo <jennifertan2009@gmail.com>
2021-02-21 18:09:16 -0800

    Update bendingMomentStress.m

--- bendingMomentStress.m
+++ bendingMomentStress.m
@@ -1,5 +1,7 @@
 % Calculates the bending moment stress
+% My in lbf ft, r in inches, Iy in in^4
 function [sigmaV] = bendingMomentStress(My,r,Iy)
-    sigmaV = (My * r)/Iy;
+    My_in = My * 12;
+    sigmaV = ((My_in * r)/Iy)/1000; %ksi
 end
 
