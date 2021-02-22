a6d0bd68738ebe39ec725ac521061297cc8e96e3
doneamo <jennifertan2009@gmail.com>
2021-02-21 18:09:19 -0800

    Update hoopStress.m

--- hoopStress.m
+++ hoopStress.m
@@ -1,6 +1,9 @@
 % Calculates the hoop stress, circumferencial and logitudinal
+% P in lbf/ft^2, r in ft, tReq in inches
 function [sigmaH,sigmaL] = hoopStress(p,r,t)
-    sigmaH = (p*r)/t;
-    sigmaL = (p*r)/(2*t);
+    P_in = p/144; %lbf/in^2
+    r_in = r * 12; %inches
+    sigmaH = ((P_in*r_in)/t)/1000; %ksi
+    sigmaL = ((P_in*r_in)/(2*t))/1000; %ksi
 end
 
