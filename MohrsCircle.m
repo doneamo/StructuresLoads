34cf5a2990a47c61b2e3dbaa5f207c1350ef119a
doneamo <jennifertan2009@gmail.com>
2021-02-21 18:09:25 -0800

    Update MohrsCircle.m

--- MohrsCircle.m
+++ MohrsCircle.m
@@ -1,17 +1,26 @@
 % calculates the maximum shear stress and principal stresses using Mohrs
 % circle method
-% All inputs in ksi
-% Theta output is in degrees
+% All inputs in ksi, except P in lbf/ft^2
+% Theta output is in degrees, positive = CW, negative = CCW as with Mohr's
+% circle convention
 
-function [tau_max,thetaP,thetaTau,sigma1,sigma2] = MohrsCircle(tau_xy,sigma_x,sigma_y)
+function [tau_max,thetaP,thetaTau,sigma1,sigma2,sigma3] = MohrsCircle(tau_xy,sigma_x,sigma_y,P)
     
     thetaP = (atand(tau_xy/((sigma_x-sigma_y)/2)))/2; %principal stress theta
     thetaTau = (atand((-(sigma_x-sigma_y)/2)/tau_xy))/2; %max shear stress theta
     
-    tau_max = sqrt(((sigma_x-sigma_y)/2)^2 + tau_xy^2);
+    R12 = sqrt(((sigma_x-sigma_y)/2)^2 + tau_xy^2);
     
-    sigma1 = (sigma_x + sigma_y)/2 + tau_max;
-    sigma2 = (sigma_x + sigma_y)/2 - tau_max;
+    sigma1 = (sigma_x + sigma_y)/2 + R12;
+    sigma2 = (sigma_x + sigma_y)/2 - R12;
+    sigma3 = -P/144/1000; %ksi
+    
+    if (sigma3 <= sigma2)
+        R13 = (sigma1 - sigma3)/2;
+        tau_max = R13;
+    else
+        tau_max = R12;
+    end
 
 end
 
