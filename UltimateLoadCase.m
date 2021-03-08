f86c7f756f5ac898322b67aaaba59557ed59df38
doneamo <jennifertan2009@gmail.com>
2021-03-07 19:31:50 -0800

    Update UltimateLoadCase.m

--- UltimateLoadCase.m
+++ UltimateLoadCase.m
@@ -1,11 +1,13 @@
 %Ultimate load case, must run limit load case first
 Fcy = 48; %ksi
+Ftu = 68; %ksi
 E = 10.9e3; %ksi
-tStr = 0.0571; %inches
+tStr = 0.05943; %inches
 B = 4.05;
 m = 0.82;
-Along = Ast; %longeron area
-tLong = tStr; %longeron thickness
+Along = Ast*1.25; %longeron area
+tLong = tStr*1.25; %longeron thickness, inches, determined by stiffener cross section
+My_ult = 5810780; %ftlbf ultimate case
 
 % crippling stress of z or c formed sections
 %stringer
@@ -18,7 +20,11 @@ sigma_cr_long = B*((1/(Along/tLong^2))^m)*((E^m * Fcy^(3-m))^(1/3));
 %current: four corners
 longLocs = zeros(1,4);
 for i = 1:1:4
-    longLocs(i) = (booms/4)*i;
+    if i ~= 4
+        longLocs(i) = (booms/4)*i +1;
+    else
+        longLocs(i) = (booms/4)*i +1 - booms;
+    end
 end
 
 %effectiveness factor
@@ -26,7 +32,8 @@ est = zeros(1,booms);
 
 %area of stinger defined
 AStrUlt = zeros(1,booms);
-for i = 1:1:booms
+%compression side
+for i = 1:1:booms/2
     if (i == longLocs(1) || i == longLocs(2) || i == longLocs(3) || i == longLocs(4))
         AStrUlt(i) = Along;
         est(i) = 1;
@@ -35,13 +42,22 @@ for i = 1:1:booms
         est(i) = sigma_cr_str/sigma_cr_long;
     end
 end
+%tension side
+for i = booms/2+1:1:booms
+    est(i) = 1;
+    if (i == longLocs(1) || i == longLocs(2) || i == longLocs(3) || i == longLocs(4))
+        AStrUlt(i) = Along;
+    else
+        AStrUlt(i) = Ast;
+    end
+end
 
 %effective area around fuselage
 Aeff = zeros(1,booms);
 for i = 1:1:booms/2
     Aeff(i) = est(i)*(AStrUlt(i) + 1.90*tSkin^2*sqrt(E/sigma_cr_str));
 end
-for i = booms/2:1:booms
+for i = booms/2+1:1:booms
     Aeff(i) = AStrUlt(i) + ((tSkin*b)/6)*(4 + z(3)/z(2) + z(1)/z(2));
 end
 
@@ -51,6 +67,7 @@ for i = 1:1:booms
     AeffWeight(i) = Aeff(i)*density; %lb/in
 end
 xSectWeight = sum(AeffWeight); %lb/in, weight in one fuselage cross section
+fprintf('total cross sectional weight is %f lbf/in \n', xSectWeight);
 
 %moment of inertia calc
 Iy_ult = 0;
@@ -61,7 +78,7 @@ end
 %bending moment stress calc
 bendStress = zeros(1,booms);
 for i = 1:1:booms
-    bendStress(i) = ((My*12) * z(i))/Iy_ult; %lbf/in^2
+    bendStress(i) = (((My_ult*12) * z(i))/Iy_ult)*(-1); %lbf/in^2, neg in compression
 end
 
 %actual bending stress with effectiveness factor
@@ -73,15 +90,18 @@ end
 %determine allowable stress in each stiffener, compression on top
 allowStress = zeros(1,booms);
 for i = 1:1:booms
-    if i < booms/2
+    if i <= booms/2
         allowStress(i) = Fcy*1000; %psi
     else
-        allowStress(i) = Ft*1000; %psi
+        allowStress(i) = Ftu*1000; %psi
     end
 end
 
 %determine margin of safety
 MS = zeros(1,booms);
 for i = 1:1:booms
-    MS(i) = allowStress(i)/actualStress(i) - 1;
+    MS(i) = allowStress(i)/abs(actualStress(i)) - 1;
 end
+
+minMS = min(MS);
+fprintf('Minimum MS is %f \n', minMS);
